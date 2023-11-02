5a458b0e2b24800f951ab030e5b41be429aa792e
Saygon96 <98422604+Saygon96@users.noreply.github.com>
2023-11-01 19:58:06 -0600

    DESARROLLADOR

---
+++ TecnoFam/Sprint 2/DESARROLLADOR SAYGON CATALÁN HERNÁNDEZ/27102023_DOR_FUNCIONALIDAD_ADMIN_CG_2023_V05/perfilespecialista.php
@@ -0,0 +1,87 @@
+<?php
+// Inicia o reanuda la sesión
+session_start();
+
+// Si el usuario no está autenticado, redirige al formulario de inicio de sesión
+if (!isset($_SESSION["usuario_autenticado"])) {
+    header("Location: login.html");
+    exit();
+}
+
+// Datos de conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "App-Adult-May";
+$database = "centro_gerontologico";
+
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Obtén el nombre del administrador desde la base de datos
+if (isset($_SESSION['matricula'])) {
+    $matricula = $_SESSION['matricula'];
+
+    $query = "SELECT nombre FROM administrador WHERE matricula = '$matricula'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $nombre = $row['nombre'];
+    }
+}
+
+// Cierra la conexión a la base de datos
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/perfil.css">
+    <link rel="shortcut icon" href="assets/iconos/icono.png">
+    <title>Centro Gerontológico Integral</title>
+</head>
+<body>
+    <nav>
+        <div class="menu-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="logo">
+            <img src="assets/logo/logo.png">
+            <div>
+                <h1 class="logo-text">Centro gerontológico integral</h1>
+                <p>Horarios de 10:00 am a 03:00 pm</p>
+            </div>
+        </div>
+        <ul class="menu">
+            <div class="profile">
+                <div class="profile-name">Especialista: <?php echo $nombre; ?></div>
+                <div class="floating-menu">
+                    <ul class="profile-options">
+                        <li><a href="cerrarsesion.php">Cerrar Sesión</a></li>
+                    </ul>
+                </div>
+            </div>
+        </ul>
+    </nav>
+
+    <div class="content">
+        <div class="galeri">
+            <h1>Asistencias</h1>
+        </div>
+    </div>
+
+    <footer>
+        Carretera Mexico- Laredo s/n, esq, con av. Insurgentes, instalaciones del antiguo patrimonio, Ixmiquilpan, Hgo., Ixmiquilpan, Mexico, 42300
+    </footer>
+</body>
+</html>
